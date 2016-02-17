4edcd6e66b9ebe624af9a20843db0263dcd86aed
Francesco Strozzi <francesco.strozzi@gmail.com>
2016-02-17 18:04:50 +0100

    modified installer script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-conda create -n vim python=2.7.11
+conda create -n vim -y python=2.7.11
 source activate vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 conda install -y cmake
-cd .vim/bundle/YouCompleteMe && ./install.py
+cd ~/.vim/bundle/YouCompleteMe && ./install.py
