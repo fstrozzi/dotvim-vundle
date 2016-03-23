7f2f9da1982c282784bd40647ba6f4ce9d7b0d66
Francesco Strozzi <fstrozzi@entmac008.enterome.net>
2016-03-23 16:34:27 +0100

    removed dedicated environment for vim installation

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-conda create -n vim -y python=2.7.11
-source activate vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 conda install -y cmake
