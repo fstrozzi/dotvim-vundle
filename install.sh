e6177846ede9474084255fb13fa0c772d029adbd
Francesco Strozzi <francesco.strozzi@gmail.com>
2017-05-26 12:35:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 conda install -y cmake
 cd ~/.vim/bundle/YouCompleteMe && ./install.py
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
